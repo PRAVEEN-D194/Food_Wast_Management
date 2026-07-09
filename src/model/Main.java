c73c9e5e8df7e9554db2cb41859afe34820346b7
PRAVEEN-D194 <praveend12321@gmail.com>
2026-07-09 18:49:33 +0530

    add donor simply

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,4 +1,5 @@
 package model;
+import com.mysql.cj.protocol.Resultset;
 import db.dbconnection;
 import model.Donor;
 import  java.sql.*;
@@ -14,57 +15,149 @@ public class Main {
     public static void main(String[] args) {
         Scanner choice = new Scanner(System.in);
         int ch;
+        String id;
+        String password;
         try{
             con =  dbconnection.getConnection();
         }catch (Exception e){
             System.out.println("db connection failed");
         }
-        do{
-            System.out.println("Welcome to Food Waste Management");
-            System.out.println("Please Enter Your Choice");
-            System.out.println("1. Add Food");
-            System.out.println("2. Add Donor");
-            System.out.println("3. View Avilable Food");
-            System.out.println("4. Allocate Food");
-            System.out.println("5. Manage Expired Food");
-            System.out.println("6. View Donors");
-            System.out.println("7. Exit");
-            System.out.println("8. View Foods");
+        do {
+
+            System.out.println("Enter Your Roll");
+            System.out.println("1.Admin");
+            System.out.println("2.User");
+            System.out.println("3.Exit");
             System.out.println(" Enter Your Choice:");
+
             ch = choice.nextInt();
-            switch (ch){
+            switch (ch) {
                 case 1:
-                    addFood();
+                    System.out.println("Enter Admin Id:");
+                    id = choice.next();
+                    System.out.println("Enter Admin Password:");
+                    password = choice.next();
+                    if(id.equals("Admin") && password.equals("12321")){
+                        admin();
+                    }else{
+                        System.out.println("wrong id or password");
+                    }
                     break;
                 case 2:
-                    addDonor();
+                    user();
                     break;
                 case 3:
-                    System.out.println("View Avilable Food");
+                    System.out.println("exit");
                     break;
-                case 4:
-                    System.out.println("Allocate Food");
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }while (ch!=3);
+//            switch (ch){
+//                case 1:
+//                    addFood();
+//                    break;
+//                case 2:
+//                    addDonor();
+//                    break;
+//                case 3:
+//                    viewFood();
+//                    break;
+//                case 4:
+//                    allocatefood();
+//                    break;
+//                case 5:
+//                    System.out.println("Manage Expired Food");
+//                    break;
+//                case 6:
+//                    viewDonor();
+//                    break;
+//                case 7:
+//                    System.out.println("Exit");
+//                    break;
+//                default:
+//                    System.out.println("Enter valid Choice");
+//                    break;
+//
+//            }
+//        }while (ch != 7);
+    }
+
+    public static void admin(){
+        Scanner scanner = new Scanner(System.in);
+        int c;
+
+        do {
+            System.out.println("Welcome to Food Waste Management");
+            System.out.println("Please Enter Your Choice");
+            System.out.println("1. view Food");
+            System.out.println("2. view Donor");
+            System.out.println("3. Allocate Food");
+            System.out.println("4. Manage Expired Food");
+            System.out.println("5. logout");
+
+            c = scanner.nextInt();
+
+            switch (c) {
+                case 1:
+                    viewFood();
                     break;
-                case 5:
-                    System.out.println("Manage Expired Food");
-                    break;
-                case 6:
+                case 2:
                     viewDonor();
                     break;
-                case 7:
-                    System.out.println("Exit");
+                case 3:
+                    allocatefood();
                     break;
-                case 8:
-                    viewFood();
+                case 4:
+                    System.out.println("Manage Expired Food page");
+                    break;
+                case 5:
+                    System.out.println("logouted successfully");
                     break;
                 default:
                     System.out.println("Enter valid Choice");
                     break;
-
             }
-        }while (ch != 7);
+        }while(c!=5);
     }
 
+
+    public static void user(){
+        Scanner scanner = new Scanner(System.in);
+        int c;
+//        System.out.println("Your Have account login else signup");
+//        System.out.println("1. Login");
+//        System.out.println("2. Signup");
+//        c=scanner.nextInt();
+        do{
+            System.out.println("Please Enter Your Choice");
+            System.out.println("1. Add Food");
+            System.out.println("2. View Avilable Food");
+            System.out.println("3. Select Food");
+            System.out.println("4. LogOut");
+
+            c = scanner.nextInt();
+
+            switch (c){
+                case 1:
+                    addFood();
+                    break;
+                case 2:
+                    viewFood();
+                    break;
+                case 3:
+                    allocatefood();
+                    break;
+                case 4:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Enter valid Choice");
+                    break;
+            }
+        }while(c!=4);
+
+    }
     public static void addDonor(){
         try {
 
@@ -101,7 +194,7 @@ public class Main {
             String foodname = s.nextLine();
             System.out.println("Enter Food Quantity:");
             String  quantity = s.nextLine();
-            System.out.println("Enter Expiry Date:");
+            System.out.println("Enter Expiry Date('YYYY-MM-DD'):");
             String expiry = s.nextLine();
 
 
@@ -158,4 +251,47 @@ public class Main {
             System.out.println("view food failed");
         }
     }
+
+    public static void allocatefood(){
+        try {
+            String query = "select * from food where allocated = false and expirydate > NOW();";
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            int foodid=0;
+            Scanner s = new Scanner(System.in);
+            int i=0;
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("food_id") + " -->  " +
+                        rs.getString("foodname") + " " +
+                        rs.getInt("quantity") + " " +
+                        rs.getString("expirydate") + " " +
+                        rs.getBoolean("allocated")
+                );
+                i++;
+            }
+            if(i!=0) {
+                System.out.println("enter what You have");
+                foodid = s.nextInt();
+
+                String q = "update food set allocated = true where food_id = ?";
+
+                PreparedStatement p = con.prepareStatement(q);
+                p.setInt(1, foodid);
+                int row = p.executeUpdate();
+
+                if (row > 0) {
+                    System.out.println("Food allocation updated successfully");
+                } else {
+                    System.out.println("Food allocation updated failed Retry");
+                }
+            }else{
+                System.out.println("Food Not Avilable For Eatable");
+            }
+
+        }catch (Exception e){
+            System.out.println("view food failed");
+        }
+    }
+
 }
